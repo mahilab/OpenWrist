a0e15f3069e7e3e9db69a0febb6a64afadedfdb2
epezent <epezent@rice.edu>
2018-05-06 10:07:16 -0500

    498 xwing

---
+++ src/Games/XWing/XWing.cpp
@@ -0,0 +1,184 @@
+#include "XWing.hpp"
+#include <MEL/Utility/System.hpp>
+#include <MEL/Utility/Windows/Keyboard.hpp>
+#include <MEL/Math/Functions.hpp>
+
+XWing::XWing(Timer timer, Q8Usb& ow_daq, OpenWrist& ow, ctrl_bool& stop_flag) :
+    StateMachine(4),
+    timer_(timer),
+    ow_daq_(ow_daq),
+    ow_(ow),
+    stop_flag_(stop_flag),
+    state_(6),
+    ow_state_("ow_state"),
+    impulses_("impulses"),
+    impulse_data_(13),
+    impulse_data_temp_(13),
+    pd0(5, 0.5),
+    pd1(5, 0.5),
+    pd2(5, 0.125)
+{
+    sleep(seconds(0.1));
+}
+
+void XWing::sf_start(const NoEventData*) {
+    // enable OpenWrist DAQ
+    ow_daq_.enable();
+    if (!ow_daq_.is_enabled()) {
+        event(ST_STOP);
+        return;
+    }
+    // enable OpenWrist
+    timer_.restart();
+    ow_.enable();
+    ow_daq_.watchdog.start();
+    // transition state
+    event(ST_PLAY);
+}
+
+void XWing::sf_play(const NoEventData*) {
+    // loop
+    while (true) {
+        // read OpenWrist DAQ
+        ow_daq_.update_input();
+        ow_daq_.watchdog.kick();
+
+
+
+        // calculate and set compensation torques
+        //ow_[0].set_torque(ow_.compute_gravity_compensation(0) + ow_.compute_friction_compensation(0));
+        //      ow_[1].set_torque(ow_.compute_gravity_compensation(1) + ow_.compute_friction_compensation(1));
+        //      ow_[2].set_torque(ow_.compute_friction_compensation(2) * 0.5);
+
+
+        tau0 = pd0.move_to_hold(0, ow_[0].get_position(), 30 * DEG2RAD, ow_[0].get_velocity(), 0.001, DEG2RAD, 30 * DEG2RAD);
+        tau1 = pd1.move_to_hold(0, ow_[1].get_position(), 30 * DEG2RAD, ow_[1].get_velocity(), 0.001, DEG2RAD, 30 * DEG2RAD);
+        tau2 = pd2.move_to_hold(0, ow_[2].get_position(), 30 * DEG2RAD, ow_[2].get_velocity(), 0.001, DEG2RAD, 30 * DEG2RAD);
+
+
+
+        ow_[0].set_torque(tau0);
+        ow_[1].set_torque(tau1);
+        ow_[2].set_torque(tau2);
+
+
+        impulse_data_ = impulses_.read_data();
+        double sum_impulse = sum(impulse_data_);
+
+
+        double k1 = 4;
+        double k2 = 2;
+        double k3 = 2;
+        double F1 = 0.25;
+        double F2 = 0.5;
+        double F3 = 0.75;
+        double F4 = 1;
+        double d1 = 1;
+        double d2 = 0.5;
+        double d3 = 0.25;
+
+        if (sum_impulse > 0.0) {
+            impulse_data_temp_ = impulse_data_;
+            pulse_clock_.restart();
+        }
+
+        if (pulse_clock_.get_elapsed_time() < seconds(0.05)) {
+            if (impulse_data_temp_[0] == 1.0) {
+                ow_[0].add_torque(-k1*F1*d1);
+                ow_[1].add_torque(k2*F1);
+                ow_[2].add_torque(-k3*F1);
+            }
+            if (impulse_data_temp_[1] == 1.0) {
+                ow_[0].add_torque(-k1*F2*d2);
+                ow_[1].add_torque(k2*F2);
+                ow_[2].add_torque(-k3*F2);
+            }
+            if (impulse_data_temp_[2] == 1.0) {
+                ow_[0].add_torque(-k1*F3*d3);
+                ow_[1].add_torque(k2*F3);
+                ow_[2].add_torque(-k3*F3);
+            }
+            if (impulse_data_temp_[3] == 1.0) {
+                ow_[0].add_torque(k1*F1*d1);
+                ow_[1].add_torque(k2*F1);
+                ow_[2].add_torque(k3*F1);
+            }
+            if (impulse_data_temp_[4] == 1.0) {
+                ow_[0].add_torque(k1*F2*d2);
+                ow_[1].add_torque(k2*F2);
+                ow_[2].add_torque(k3*F2);
+            }
+            if (impulse_data_temp_[5] == 1.0) {
+                ow_[0].add_torque(k1*F3*d3);
+                ow_[1].add_torque(k2*F3);
+                ow_[2].add_torque(k3*F3);
+            }
+            if (impulse_data_temp_[6] == 1.0) {
+                ow_[0].add_torque(k1*F1*d1);
+                ow_[1].add_torque(-k2*F1);
+                ow_[2].add_torque(-k3*F1);
+            }
+            if (impulse_data_temp_[7] == 1.0) {
+                ow_[0].add_torque(k1*F2*d2);
+                ow_[1].add_torque(-k2*F2);
+                ow_[2].add_torque(-k3*F2);
+            }
+            if (impulse_data_temp_[8] == 1.0) {
+                ow_[0].add_torque(k1*F3*d3);
+                ow_[1].add_torque(-k2*F3);
+                ow_[2].add_torque(-k3*F3);
+            }
+            if (impulse_data_temp_[9] == 1.0) {
+                ow_[0].add_torque(-k1*F1*d1);
+                ow_[1].add_torque(-k2*F1);
+                ow_[2].add_torque(k3*F1);
+            }
+            if (impulse_data_temp_[10] == 1.0) {
+                ow_[0].add_torque(-k1*F2*d2);
+                ow_[1].add_torque(-k2*F2);
+                ow_[2].add_torque(k3*F2);
+            }
+            if (impulse_data_temp_[11] == 1.0) {
+                ow_[0].add_torque(-k1*F3*d3);
+                ow_[1].add_torque(-k2*F3);
+                ow_[2].add_torque(k3*F3);
+            }
+            if (impulse_data_temp_[12] == 1.0) {
+                ow_[2].add_torque(k3*F4);
+            }
+        }
+
+        for (int i = 0; i<13; i = i + 1) {
+            impulses_.write_data({0,0,0,0,0,0,0,0,0,0,0,0,0});
+        }
+
+
+        // update positions/velocities
+        state_[0] = ow_[0].get_position();
+        state_[1] = ow_[1].get_position();
+        state_[2] = ow_[2].get_position();
+        state_[3] = ow_[0].get_velocity();
+        state_[4] = ow_[1].get_velocity();
+        state_[5] = ow_[2].get_velocity();
+        ow_state_.write_data(state_);
+        // check for stop conditions
+        if (stop_flag_ ||
+            ow_.any_limit_exceeded() ||
+            Keyboard::is_key_pressed(Key::Escape)) {
+            event(ST_STOP);
+            return;
+        }
+        // write OpenWrist DAQ
+        ow_daq_.update_output();
+        // wait Clock
+        timer_.wait();
+    }
+}
+
+void XWing::sf_stop(const NoEventData*) {
+    // disable OpenWrist
+    ow_.disable();
+    // disable OpenWrist DAQ
+    ow_daq_.watchdog.stop();
+    ow_daq_.disable();
+}
---
+++ src/Games/XWing/XWing.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include <MEL/Core/Timer.hpp>
+#include <MEL/Utility/StateMachine.hpp>
+#include "OpenWrist.hpp"
+#include <MEL/Communications/MelNet.hpp>
+#include <MEL/Daq/Quanser/Q8Usb.hpp>
+#include <MEL/Utility/Console.hpp>
+#include <MEL/Communications/Windows/MelShare.hpp>
+
+
+using namespace mel;
+
+class XWing : public StateMachine {
+
+public:
+
+	XWing(Timer timer, Q8Usb& ow_daq, OpenWrist& ow, ctrl_bool& stop_flag);
+	
+private:
+
+    //-------------------------------------------------------------------------
+    // STATE MACHINE SETUP
+    //-------------------------------------------------------------------------
+
+    // STATES
+    enum States {
+        ST_START,
+        ST_PLAY,
+        ST_STOP,
+        ST_NUM_STATES
+    };
+
+    // STATE FUNCTIONS
+    void sf_start(const NoEventData*);
+    void sf_play(const NoEventData*);
+    void sf_stop(const NoEventData*);
+
+    // STATE ACTIONS
+	StateAction<XWing, NoEventData, &XWing::sf_start> sa_start;
+    StateAction<XWing, NoEventData, &XWing::sf_play>  sa_play;
+    StateAction<XWing, NoEventData, &XWing::sf_stop>  sa_stop;
+
+    // STATE MAP
+    virtual const StateMapRow* get_state_map() {
+        static const StateMapRow STATE_MAP[] = {
+            &sa_start,
+            &sa_play,
+            &sa_stop,
+        };
+        return &STATE_MAP[0];
+    }
+
+    //-------------------------------------------------------------------------
+    // PRIVATE VARIABLES
+    //-------------------------------------------------------------------------
+
+    // HARDWARE TIMER
+    Timer timer_;
+
+    // HARDWARE
+    Q8Usb& ow_daq_;
+    OpenWrist& ow_;
+
+    // STOP FLAG
+    ctrl_bool& stop_flag_;
+
+    std::vector<double> state_;
+	std::vector<double> impulse_data_;
+    std::vector<double> impulse_data_temp_;
+	
+    Clock pulse_clock_;
+
+    // MELSHARES
+	MelShare ow_state_;
+	MelShare impulses_;
+
+    double tau0, tau1, tau2;
+
+    PdController pd0, pd1, pd2;
+
+};
